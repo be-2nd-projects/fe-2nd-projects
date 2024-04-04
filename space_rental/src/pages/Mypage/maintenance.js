f9c78dc38ee0d81e284f76f91643378eee9ec050
[Github <[Gibhub>
2024-04-04 22:04:17 +0900

    mypage

---
+++ space_rental/src/pages/Mypage/maintenance.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+
+const maintenance = () => {
+    return (
+        <div>
+            <div class="container">
+                <div>My Home</div>
+                <div>문의관리</div>
+                <div>숙소 공지사항</div>
+                <div>수익현황</div>
+                <div>강의 총 수익</div>
+            </div>
+            <div class="container2">
+                <div>이번 달 현황</div>
+                <div>강의 수익 분포</div>
+            </div>
+        </div>
+    );
+};
+
+export default maintenance;
